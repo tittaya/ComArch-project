05f20f31356aeca953aee42ce07d2dd77919b7af
nickkle891 <46592687+nickkle891@users.noreply.github.com>
2020-11-16 20:15:48 +0700

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,14 +1,14 @@
-from Type_Code import J_type_GenCode,I_type_GenCode,O_type_GenCode,R_type_GenCode,gen_32twoCom
+import CodeType as c
 
 def Assembler(lineSplit,mem,PC,label_addr):                                  
         if lineSplit[1] == 'add' or lineSplit[1] == 'nand'  :                          #เช็คตาม Type 
-            mem[PC] = int(R_type_GenCode(lineSplit),2)                                 #go to Type_code.py ได้เลขฐานสองมาทำเป็นเลขฐานสิบเก็บใน mem
+            mem[PC] = int(c.Rtype(lineSplit),2)                                 #go to Type_code.py ได้เลขฐานสองมาทำเป็นเลขฐานสิบเก็บใน mem
         elif  lineSplit[1] == 'lw' or lineSplit[1] == 'sw' or lineSplit[1] == 'beq' :   
-            mem[PC] = int(I_type_GenCode(lineSplit,PC,label_addr),2)
+            mem[PC] = int(c.Itype(lineSplit,PC,label_addr),2)
         elif lineSplit[1] == 'halt' or lineSplit[1] == 'noop' :
-            mem[PC] = int(O_type_GenCode(lineSplit),2)
+            mem[PC] = int(c.Otype(lineSplit),2)
         elif lineSplit[1] == 'jalr':
-            mem[PC] = int(J_type_GenCode(lineSplit),2)
+            mem[PC] = int(c.Jtype(lineSplit),2)
         elif lineSplit[1] == '.fill':
             
             if lineSplit[2].lstrip('-').isdigit():                             #ถ้าตัวหลังจาก .fill เป็นเลข เอามาใส่เลย
